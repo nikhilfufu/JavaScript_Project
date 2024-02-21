ae29bdb48051a072a85aa0e0d0c77fd6cd646484
nikhilfufu <pallaviml007@outllook.com>
2024-02-21 23:43:47 +0530

    nikhil changes 21st

---
+++ Strings2.java
@@ -0,0 +1,58 @@
+public class Strings2 {
+    public static void main(String[] args) {
+        //what is string is ?
+        // group of char and it is an object
+        // sep. class java.lang.string
+        // two ways of creating string
+        //1) 
+     /*    String s = new String("nikhil");
+        String s2 = new String();
+        s2 = "jeethu";
+        String s3 = s+s2;
+        System.out.println("s3-->"+s3+"s2-->"+s2+"s-->"+s);
+        // String is immutable
+        String s5 = "nikhil"; // a new object s will be created in the String pool with the value "nikhil"
+        int a = s5.hashCode(); // Calculate the hash code of the string "nikhil"
+        s5 = s5 + "jeethu"; // Concatenate "jeethu" to the string s. Since strings are immutable in Java, a new string object will be created in the String pool with the value "nikhiljeethu". The reference s will now point to this new string object.
+        System.out.println(s5); // Print the updated value of s
+        int b = s.hashCode(); // Calculate the hash code of the updated string 
+        if (a == b) {
+        System.out.println("string is not mutable"); // If the hash codes are the same, print that the string is not mutable
+                } 
+                else {
+                    System.out.println("a--->" + a); // Print the hash code of the original string
+                    System.out.println("b--->" + b); // Print the hash code of the updated string
+                    System.out.println("string is immmutable"); // If the hash codes are different, print that the string is mutable
+                }*/
+            
+    /*String new2 = "nikhiljeethu";
+    String new3  ="nikhiljeethu";
+    if(new2==new3){
+        System.out.println("equal");//to comapre string we use equals cause we will compare the string not the address in case of string
+    }
+    else
+        System.out.println("not equal");
+ 
+ 
+    */
+
+    String s2 ="jeethu";
+    String s1 ="nikhil";
+  /*  String[] s3 = s.split("i");
+   for (int index = 0; index < s3.length; index++) {
+    System.out.println(s3[index]);
+   }*/
+   //
+//110- //106//=4
+//106 - //110 //=-4
+
+
+
+
+
+
+
+}
+}
+
+
